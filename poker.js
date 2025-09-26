afc651d4d0513fe010774c601c2e8d7df2813de3
AnuragFrantic <anurag@franticpro.com>
2025-09-26 16:07:21 +0530

    `Refactor CORS configuration in poker.js`

--- poker.js
+++ poker.js
@@ -13,9 +13,31 @@ const masterRoutes = require("./routes/masterRoutes");
 const app = express();
 
 //  Enable CORS
+// app.use(cors({
+//     origin: ["http://localhost:5173", "http://storo.doc24.care/"],
+//     credentials: true
+// }));
+
+const allowedOrigins = ["http://localhost:5173", "http://storo.doc24.care"];
+
 app.use(cors({
-    origin: ["http://localhost:5173", "http://storo.doc24.care/"],
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // allow non-browser requests like Postman
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// Handle preflight requests
+app.options("*", cors({
+    origin: allowedOrigins,
+    credentials: true,
 }));
 
 app.use(express.json());
