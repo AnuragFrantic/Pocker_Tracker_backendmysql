92fa6ed9a8eb562f3575752dc76da204ca11875e
AnuragFrantic <anurag@franticpro.com>
2025-11-10 11:40:15 +0530

    `Updated UserGameHistory controller to return 200 status code when no records are found for a year`

--- controllers/UserGameHistory.js
+++ controllers/UserGameHistory.js
@@ -592,7 +592,7 @@ exports.generateTaxStatementPdf = async (req, res) => {
         });
 
         if (!histories.length) {
-            return res.status(404).json({ message: `No records found for year ${year}`, error: false });
+            return res.status(200).json({ message: `No records found for year ${year}`, error: false });
         }
 
         let totalBuyIns = 0;
